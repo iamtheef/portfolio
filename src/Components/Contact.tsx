6a999bbe2cdca517ab12a5c18b3f23e87964805c
iamtheef <eftihis.riise@gmail.com>
2020-05-29 21:53:33 +0300

    margin bug

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -7,7 +7,7 @@ const Contact: React.FC = () => {
   return (
     <div>
       <div className="container">
-        <div className="row paragraph" style={{ marginTop: "-550px" }}>
+        <div className="row paragraph marginTop">
           <div className="col">
             {isGreek() ? (
               <p>
