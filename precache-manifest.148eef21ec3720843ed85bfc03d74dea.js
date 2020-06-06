c2403d068bb01c9337ee995691f9e2a4bf6aa918
iamtheef <eftihis.riise@gmail.com>
2020-06-06 12:09:18 +0300

    Updates

--- precache-manifest.148eef21ec3720843ed85bfc03d74dea.js
+++ precache-manifest.148eef21ec3720843ed85bfc03d74dea.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8c6de5526d7b77e7625e82f13c4d5178",
+    "revision": "78f7176ad21337b222f45743504ec722",
     "url": "/portofolio/index.html"
   },
   {
