c67a3f87885d65b5d505c3af0cb2d69350e1cb20
iamtheef <eftihis.riise@gmail.com>
2020-12-01 00:52:53 +0200

    fixing

--- src/utils/makeTable.tsx
+++ src/utils/makeTable.tsx
@@ -5,13 +5,12 @@ let linesPerHash: any = {
   experience: Math.floor(document.documentElement.clientHeight / 17) + 2,
   about: Math.floor(document.documentElement.clientHeight / 23),
   contact: Math.floor(document.documentElement.clientHeight / 100),
-  default: Math.floor(document.documentElement.clientHeight / 100) + 2,
+  default: Math.floor(document.documentElement.clientHeight / 100),
 };
 
 export const makeTable = () => {
   const hash = window.location.hash.slice(2);
   let lines: number = linesPerHash[hash] | linesPerHash["default"];
-  console.log(linesPerHash[hash]);
   const newTable: any = [];
 
   for (let i = 0; i < lines; i++) {
