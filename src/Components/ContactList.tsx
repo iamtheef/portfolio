ec0cc592b62fd1541119b462d0d914528deba27f
iamtheef <eftihis.riise@gmail.com>
2020-12-05 00:05:45 +0200

    easter egg & autocopy

--- src/Components/ContactList.tsx
+++ src/Components/ContactList.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import tel from "../assets/tel.png";
 import mail from "../assets/mail.png";
 import discord from "../assets/discord.png";
+import { copyToClipboard } from "../utils/copyToClipboard";
 
 const ContactList: React.FC = () => {
+  const [isMailCopied, setIsMailCopied] = useState(false);
+  const [isDCopied, setIsDCopied] = useState(false);
+  const handleCopy = (e: React.ChangeEvent<any>) => {
+    if (e.target.id === "mail") {
+      setIsMailCopied(true);
+      setTimeout(() => {
+        setIsMailCopied(false);
+      }, 1000);
+      copyToClipboard("iamtheef_th@protonmail.com");
+    } else {
+      setIsDCopied(true);
+      setTimeout(() => {
+        setIsDCopied(false);
+      }, 1000);
+      copyToClipboard("Th#1833");
+    }
+  };
+
   return (
     <div className="contact-list">
       <div className="container">
         <div className="col-12 float-left">
           <ul className="nobull" style={{ marginTop: "30px" }}>
-            <li>
-              {" "}
+            <li onClick={handleCopy}>
               <img src={mail} alt="mail icon" className="footer-icon" />
-              iamtheef_th@protonmail.com
+              <span className={`${isMailCopied && "blink"}`} id="mail">
+                {isMailCopied ? "mail copied" : "iamtheef_th@protonmail.com"}
+              </span>
             </li>
             <li>
               {" "}
@@ -20,10 +40,12 @@ const ContactList: React.FC = () => {
               +30 6980970012 (WhatsApp, Viber)
             </li>
 
-            <li>
+            <li onClick={handleCopy}>
               {" "}
               <img src={discord} alt="dscr" className="footer-icon" />
-              Discord Th#1833
+              <span className={`${isDCopied && "blink"}`} id="discord">
+                {isDCopied ? "discord copied" : "Discord Th#1833"}
+              </span>
             </li>
           </ul>
         </div>
